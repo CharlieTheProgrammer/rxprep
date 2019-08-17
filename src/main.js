5dd00bee5ac7a4f5e95cd9c007f86a2f62f16c1e
Carlos Ontiveros <charlietheprogrammer@outlook.com>
2019-08-17 12:44:53 -0700

    Added Bootstrap Vue and vuejs-datepicker components.

--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ Vue.config.productionTip = false
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
